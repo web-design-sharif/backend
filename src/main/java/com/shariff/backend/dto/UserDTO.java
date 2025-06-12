25d90ddfb7ff6f4b212383892d070361dd44636a
Hoora <hooraabedin2004@gmail.com>
2025-06-12 22:57:45 +0330

    fix DTO

--- src/main/java/com/shariff/backend/dto/UserDTO.java
+++ src/main/java/com/shariff/backend/dto/UserDTO.java
@@ -1,5 +1,14 @@
 package com.shariff.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private int id;
     private String email;
